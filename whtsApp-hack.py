7cc0112b0ae7a53596007d52d84979d20f01c980
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-02-15 19:28:10 +0300

    Add files via upload

---
+++ whtsApp-hack.py
@@ -0,0 +1,14 @@
+import os 
+os.system ("clear")
+v7x = input("Enter the country code:")
+print (v7x)
+v7x2 = input("Enter the victim number:")
+print (v7x2)
+print ("The number is being verified")
+print ("The verification code is guessed")
+list100=0
+while list100 < 100000 :
+    list100=list100+1
+    print (list100)
+print ("The verification code is being generated, please wait")
+print ("====>>956002")
